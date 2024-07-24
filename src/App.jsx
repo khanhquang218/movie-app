17335312696eb6f68a4c3987bd1feda6ff452bcf
Khanh Quang <khanhquang213@gmail.com>
2024-07-24 23:00:02 +0700

    update carousel
    Cài thư viện carousel để thay thế cho slice MovieList

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
 			<div className="bg-black pb-10">
 				<Header />
 				<Banner />
-				<MovieList title={'Phim Hot'} data={movie.slice(0, 5)} />
-				<MovieList title={'Phim Đề Cử'} data={movieRate.slice(0, 5)} />
+				<MovieList title={'Phim Hot'} data={movie} />
+				<MovieList title={'Phim Đề Cử'} data={movieRate} />
 			</div>
 		</>
 	);
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ImgTemp from '../assets/temp-1.jpeg';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+//setup carousel
+const responsive = {
+	superLargeDesktop: {
+		// the naming can be any, depends on you.
+		breakpoint: { max: 4000, min: 3000 },
+		items: 10,
+	},
+	desktop: {
+		breakpoint: { max: 3000, min: 1200 },
+		items: 7,
+	},
+	tablet: {
+		breakpoint: { max: 1200, min: 600 },
+		items: 3,
+	},
+	mobile: {
+		breakpoint: { max: 600, min: 0 },
+		items: 2,
+	},
+};
 
 const MovieList = ({ title, data }) => {
 	return (
 		<div className="text-white p-10 mb-10">
 			<h2 className="uppercase text-xl mb-4">{title}</h2>
-			<div className="flex items-center space-x-4">
+			<Carousel responsive={responsive} className="flex items-center space-x-4">
 				{data.length > 0 &&
 					data.map((item) => {
 						return (
@@ -21,7 +43,7 @@ const MovieList = ({ title, data }) => {
 							</div>
 						);
 					})}
-			</div>
+			</Carousel>
 		</div>
 	);
 };
