7f7c615f78eb8b92704331ce665380ccf900db9f
Khanh Quang <khanhquang213@gmail.com>
2024-07-27 17:49:30 +0700

    update

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import Modal from 'react-modal';
+import YouTube from 'react-youtube';
+
+//config modals
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
+};
+
+//setup react-youtube
+const opts = {
+	height: '390',
+	width: '640',
+	playerVars: {
+		// https://developers.google.com/youtube/player_parameters
+		autoplay: 1,
+	},
+};
 
 //setup carousel
 const responsive = {
@@ -24,26 +48,53 @@ const responsive = {
 	},
 };
 
+//Kh nên sử dụng modal nằm trong map, vì sẽ render rất nhiều lần
 const MovieList = ({ title, data }) => {
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [trailerKey, setTrailerKey] = useState('');
+
+	const handleTrailer = async (id) => {
+		setTrailerKey('');
+		try {
+			const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
+			const options = {
+				method: 'GET',
+				headers: {
+					accept: 'application/json',
+					Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+				},
+			};
+
+			const movieKey = await fetch(url, options);
+			const data = await movieKey.json();
+			setTrailerKey(data.results[0].key);
+			setModalIsOpen(true);
+		} catch (error) {
+			setModalIsOpen(false);
+			console.log(error);
+		}
+	};
 	return (
 		<div className="text-white p-10 mb-10">
 			<h2 className="uppercase text-xl mb-4">{title}</h2>
 			<Carousel responsive={responsive} className="flex items-center space-x-4">
 				{data.length > 0 &&
-					data.map((item) => {
-						return (
-							<div key={item.id} className="w-[200px] h-[300px] relative group">
-								<div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
-									<div className="absolute top-0 left-0 w-full h-full bg-black/40" />
-									<img src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`} alt={item.title} className="w-full h-full object-cover" />
-									<div className="absolute bottom-4 left-2">
-										<p className="uppercase text-md">{item.title || item.original_title}</p>
-									</div>
+					data.map((item) => (
+						<div key={item.id} className="w-[200px] h-[300px] relative group" onClick={() => handleTrailer(item.id)}>
+							<div className="group-hover:scale-105 transition-transform duration-500 ease-in-out w-full h-full cursor-pointer">
+								<div className="absolute top-0 left-0 w-full h-full bg-black/40" />
+								<img src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`} alt={item.title} className="w-full h-full object-cover" />
+								<div className="absolute bottom-4 left-2">
+									<p className="uppercase text-md">{item.title || item.original_title}</p>
 								</div>
 							</div>
-						);
-					})}
+						</div>
+					))}
 			</Carousel>
+
+			<Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} style={customStyles} contentLabel="Example Modal">
+				{/* <YouTube videoId={trailerKey} opts={opts} onReady={this._onReady} />; */}
+			</Modal>
 		</div>
 	);
 };
